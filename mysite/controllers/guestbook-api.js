cfa01898745e0ad23b616691b8b76bcb8e14d109
jeonghyun051 <rlawjdgus051@naver.com>
2021-07-16 16:12:44 +0900

    api guestbook spa solved

--- mysite/controllers/guestbook-api.js
+++ mysite/controllers/guestbook-api.js
@@ -1,49 +1,64 @@
+const models = require('../models');
+const moment = require('moment');
+const { Sequelize } = require('sequelize');
+
 module.exports = {
     create: async function(req, res, next) {
-        console.log(req.body);
-        // sql insert
-        res.status(200).send({
+        try {        
+            await models.Guestbook.create(req.body);
+            res.status(200).send({
             result: 'success',
-            data: Object.assign(req.body, {
-                no: 10,
-                password: '',
-                regDate: new Date()
-            }),
+            data: (req.body), // Object.assign : 복사 기능
             message: null
         });
+        } catch(e) {
+            next(e);
+        }     
     },
     read: async function(req, res, next) {
-        const startNo = req.query.sno || 0;
-        console.log(startNo);
+        const no = req.params.no || 0;
+
         // sql: select.... limit
-        res.status(200).send({
-            result: 'success',
-            data:[{
-                no: 9,
-                name: '둘리',
-                message: '호이~',
-                regDate: new Date()
-            }, {
-                no: 8,
-                name: '마이콜',
-                message: '구공탄~',
-                regDate: new Date()
-            }, {
-                no: 7,
-                name: '도우너',
-                message: '둘리야~',
-                regDate: new Date()
-            }],
-            message: null
-        });
+        if(no == 0){
+            const results = await models.Guestbook.findAll({
+                order: [['no','DESC']],
+                offset: 0, limit: 3
+            });
+            res.status(200).send({
+                result: 'success',
+                data: results || [],
+                message: null
+            });
+        } else {
+            const results = await models.Guestbook.findAll({   
+                where:{
+                    no: { [Sequelize.Op.lt] : no }  
+                },
+                order: [['no','DESC']],
+                offset: 0, limit: 3
+            });
+            res.status(200).send({
+                result: 'success',
+                data: results || [],
+                message: null
+            });
+        }
     },
-    delete: function(req, res, next) {
-        console.log(req.params.no + ":" + req.body.password);
-        // sql delete
-        res.status(200).send({
-            result: 'success',
-            data: req.params.no,
-            message: null
-        });
+    delete: async function(req, res, next) {
+        try {        
+            const result = await models.Guestbook.destroy({ // 성공하면 1, 실패하면 0
+                where: {
+                    no: req.params.no,
+                    password: req.body.password
+                }
+            });
+            res.status(200).send({
+                result: 'success',
+                data: result == 1 ? req.params.no : -1, 
+                message: null
+            });
+        } catch(e) {
+            next(e);
+        }     
     }
 }
--- mysite/controllers/guestbook.js
+++ mysite/controllers/guestbook.js
@@ -25,7 +25,7 @@ module.exports = {
             }
         });
         res.redirect('/guestbook');
-    },
+    }, 
     add: async function(req, res, next) {
         try {        
             await models.Guestbook.create(req.body);
@@ -33,5 +33,8 @@ module.exports = {
         } catch(e) {
             next(e);
         }        
+    },
+    spa: function(req, res){
+        res.render('guestbook/spa-landing'); 
     }
 }
